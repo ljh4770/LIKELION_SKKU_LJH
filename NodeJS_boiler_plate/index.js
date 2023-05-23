4d1713322809b38f16945819e2b6f42b00a48e5b
JungHwan_Laptop <ljh4770@naver.com>
2023-05-23 12:06:23 +0900

    register

--- NodeJS_boiler_plate/index.js
+++ NodeJS_boiler_plate/index.js
@@ -4,6 +4,16 @@ const express = require('express');
 // 모듈의 이름을 express로 선언
 const app = express();
 const port = 5000;
+const { User } = require('./models/User');
+const { mongoURI } = require('./config/key')
+
+const bodyParser = require('body-parser');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+// 위 주석 형식의 데이터를 분석해서 가져올 수 있게 함
 
 const mongoose = require('mongoose');
 // mongoDB 데이터베이스 -> json
@@ -11,23 +21,57 @@ const mongoose = require('mongoose');
 // SQL -> 전통적으로 데이터베이스를 다루고, 정보들에 접근하고 변경하고
 // Relation == Table == 특성에 맞게 분리해줘야함 // 제1정규화 2정규화
 // noSQL 
-mongoose.connect('mongodb+srv://ljh640370:qwer1234@cluster0.vbe3cnu.mongodb.net/', {
+mongoose.connect(mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true // useCreateIndex: true, useFindAndMOdify: false
 }).then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
 
+app.get('/', (req, res) => res.send('Hello World!\n nodemon 적용'));
+app.get('/example', (req, res) => res.send("example respond"));
+
+
+// 회원 가입을 위한 route
+app.post("/register", (req, res) =>{
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  // req.body 안에는 josn 형식에 데이터가 담겨져 있음
+  /*
+  {
+    id: "hello",
+    password: "123"
+  }
+  */
+  const user = new User(req.body);
+
+  // mongoDB method
+  // client에서 가져온 데이터를 mongDB에 저장해줌
+  /*
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err});
+    return res.status(200).json({succes: true});
+  })
+  */
+
+  // mongoDB 버전이 올라가서 callback 함수를 더 이상 지원하지 않는다고 함
+  // -> callback 함수 정의하지 않고 구현
+ try{
+  user.save()
+  .then(() => {
+    return res.status(200).json({succes: true});
+  })
+  .catch((err) => {
+    return res.json({succes: false, err});
+  })
+ } catch (err){
+  console.log(err);
+ } 
+})
 
 function handleListen(){
   console.log(`Example app listening on port ${port}`);
 }
-
-// request, respond
-// app.get('url', 함수);
-// app.listen(port, 함수);
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/example', (req, res) => res.send("example respond"));
-app.listen(port, handleListen());
+app.listen(port, handleListen);
 
 
--- NodeJS_boiler_plate/models/User.js
+++ NodeJS_boiler_plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -12,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        minlength: 50
+        minlength: 5
     },
     role: {
         type: Number,
@@ -26,6 +29,28 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function( next ){
+    var user = this;
+    // salt를 이용하여 비밀번호 암호화 진행
+
+    if (user.isModified('password')){
+        // salt 생성 (slatRounds 이용)
+        bcrypt.genSalt(saltRounds, function (err, salt){
+            // 에러 전달
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                // 에러 전달
+                if(err) return next(err);
+
+                user.password = hash;
+
+                next();
+            })
+        })
+    }
+})
 
 const User = mongoose.model('User', userSchema);
 
